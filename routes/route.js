393da6a97df4668a826195619a28c2cc246d8e4b
Sean Hornsby <sean.hornsby@gmail.com>
2016-06-28 11:27:19 -0700

    Adds route for train route and updates README with directions for the API.

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,20 @@ var route = require("express").Router();
 var schedule = require("../data/schedule").data;
 
 route.get("/", function(req, res) {
-  console.log(req.query);
-  res.send("route... route");
+  var route = req.query.route;
+  var station = req.query.station;
+  var trainObjects = schedule.filter(function(item) {
+    return item.route == route;
+  });
+  var response = trainObjects.map(function(item) {
+    if (station != undefined) {
+      return {train: item.train, time: item.stops[station]};
+    } else {
+      return {train: item.train, stops:item.stops};
+    }
+
+  });
+  res.send(response);
 });
 
 module.exports = route;
