7dc2f8e8505238847c9619d08d37b8440c444d6e
Sean Hornsby <sean.hornsby@gmail.com>
2016-06-29 21:59:24 -0700

    Train route coming together

--- routes/train.js
+++ routes/train.js
@@ -1,22 +1,51 @@
 var train = require("express").Router();
 var schedule = require("../data/schedule").data;
-var querystring = require("querystring");
+var time = require("../time.js");
 train.get("/", function(req, res) {
   console.log(req.query);
   var trainNumber = req.query.train;
   var station = req.query.station;
   var trainObject = schedule.filter(function(item) {
-
     return item.train == trainNumber;
   });
   console.log(trainObject[0].stops);
   var response = trainObject;
   if (station !== undefined) {
-
+    //response = findTrainObject(schedule, station);
+    console.log(response);
     response = trainObject[0].stops.filter((stop) => stop.name === station);
+  } else {
+    response = trainObject.map(function(item) {
+      var results = [];
+
+      for (var i = 0; i < item.stops.length; i++) {
+        var itemTime = item.stops[i].time;
+        var timeString = time.hasTime(itemTime);
+        var status;
+        if (item.variance > 0) {
+          status = "Late";
+        } else {
+          status = "On-Time";
+        }
+        var object = {train: item.train, name: item.stops[i].name, time: timeString, status: status};
+        results.push(object);
+      }
+      return results;
+
+    });
   }
-  res.send([response]);
+  res.send(response);
 });
 
+// var findTrainObject = function(trains, station) {
+//   var result = trains.map((train) => {
+//     var stops = train.stops;
+//     var stop = stops.filter((object) => {
+//       return object.station === station;
+//     });
+//     console.log(`stop ${stop}`);
+//     return {name: train.train, status: train.variance, station: stop[0].name, time: time.hasTime(stop[0].time)};
+//   });
+// };
 
 module.exports = train;
