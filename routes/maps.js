57a293d1683ed7576fe228e85d086185c72392d0
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-03 11:20:50 -0700

    API wired up and returning the proper data. #3

---
+++ routes/maps.js
@@ -0,0 +1,43 @@
+var maps = require("express").Router();
+var env = require('../ENV.js').keys[0];
+const https = require('https');
+
+maps.get("/", function(req, res) {
+  var query=req.query;
+  var data = getDirections(query, function(data) {
+    res.status(200).send(data);
+  });
+});
+
+
+
+var getDirections = function(query, callback) {
+  var origin = query.origin;
+  var destination = query.destination;
+  var key = env.directions;
+  console.log("Getting");
+  var querystring = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${key}`;
+  var finalData="";
+  var smashedChunk="";
+  var req = https.request(querystring, function (res) {
+    //console.log(res);
+    res.on("data", function(chunk) {
+      var textChunk = chunk.toString('utf8');
+      finalData = finalData + textChunk;
+      smashedChunk = smashedChunk + chunk;
+      //console.log(textChunk);
+    });
+    res.on("end", function() {
+      //callback(finalData);
+      var result = JSON.parse(smashedChunk);
+      var importantData = result.routes[0].legs[0];
+      var returnObject = {distance: importantData.distance.text, duration: importantData.duration.text};
+      callback(returnObject);
+    });
+  });
+  req.end();
+}
+
+
+
+module.exports = maps;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 var train = require("./routes/train.js");
 var search = require("./routes/search.js");
-
+var maps = require("./routes/maps.js");
 app.use(express.static("./public"));
 
 app.use("/search", search);
 app.use("/train", train);
-
+app.use("/maps", maps);
 
 
 app.listen(8000, () => console.log("Listening on port 8000"));
