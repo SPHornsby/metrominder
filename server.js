3327f36fb155d3ac67818fda753abba4451eff89
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-04 13:20:59 -0700

    Heroku setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var train = require("./routes/train.js");
 var search = require("./routes/search.js");
-var maps = require("./routes/maps.js");
+//var maps = require("./routes/maps.js");
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -10,7 +10,7 @@ app.use(express.static("./public"));
 
 app.use("/search", search);
 app.use("/train", train);
-app.use("/maps", maps);
+//app.use("/maps", maps);
 
 
 app.listen(app.get("port"), () => console.log(`Listening on port:${app.get('port')}`));
