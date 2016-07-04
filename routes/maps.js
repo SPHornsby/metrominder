8b44b1a73ce593f717d1e533d55c6c1630e07e1c
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-04 13:28:21 -0700

    Heroku setup

--- routes/maps.js
+++ routes/maps.js
@@ -1,42 +1,42 @@
-// var maps = require("express").Router();
-// var locations = require("../data/locations.js").data;
-// var env = require("../ENV.js").keys[0];
-// const https = require("https");
-// maps.get("/station", function(req, res) {
-//   var query = req.query.station;
-//   var latLong = locations.filter( (location) => location.name === query);
-//   res.send(latLong);
-// });
-// maps.get("/", function(req, res) {
-//   var query=req.query;
-//   getDirections(query, function(data) {
-//     res.status(200).send(data);
-//   });
-// });
-// var getDirections = function(query, callback) {
-//   var origin = query.origin;
-//   var destination = query.destination;
-//   var key = env.directions;
-//   var querystring = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${key}`;
-//   var finalData="";
-//   var smashedChunk="";
-//   var req = https.request(querystring, function (res) {
-//     //console.log(res);
-//     res.on("data", function(chunk) {
-//       var textChunk = chunk.toString("utf8");
-//       finalData = finalData + textChunk;
-//       smashedChunk = smashedChunk + chunk;
-//       //console.log(textChunk);
-//     });
-//     res.on("end", function() {
-//       //callback(finalData);
-//       var result = JSON.parse(smashedChunk);
-//       var importantData = result.routes[0].legs[0];
-//       var justMinutes = importantData.duration.text.split(" ")[0];
-//       var returnObject = {distance: importantData.distance.text, duration: justMinutes};
-//       callback(returnObject);
-//     });
-//   });
-//   req.end();
-// };
-// module.exports = maps;
+var maps = require("express").Router();
+var locations = require("../data/locations.js").data;
+//var env = require("../ENV.js").keys[0];
+const https = require("https");
+maps.get("/station", function(req, res) {
+  var query = req.query.station;
+  var latLong = locations.filter( (location) => location.name === query);
+  res.send(latLong);
+});
+maps.get("/", function(req, res) {
+  var query=req.query;
+  getDirections(query, function(data) {
+    res.status(200).send(data);
+  });
+});
+var getDirections = function(query, callback) {
+  var origin = query.origin;
+  var destination = query.destination;
+  var key = process.env.GD_KEY;
+  var querystring = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${key}`;
+  var finalData="";
+  var smashedChunk="";
+  var req = https.request(querystring, function (res) {
+    //console.log(res);
+    res.on("data", function(chunk) {
+      var textChunk = chunk.toString("utf8");
+      finalData = finalData + textChunk;
+      smashedChunk = smashedChunk + chunk;
+      //console.log(textChunk);
+    });
+    res.on("end", function() {
+      //callback(finalData);
+      var result = JSON.parse(smashedChunk);
+      var importantData = result.routes[0].legs[0];
+      var justMinutes = importantData.duration.text.split(" ")[0];
+      var returnObject = {distance: importantData.distance.text, duration: justMinutes};
+      callback(returnObject);
+    });
+  });
+  req.end();
+};
+module.exports = maps;
