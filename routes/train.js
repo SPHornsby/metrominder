aba5e022fa292248cb72c99fa2d7519871688f25
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-04 15:22:18 -0700

    Adds PUT route for trains path. #5

--- routes/train.js
+++ routes/train.js
@@ -12,5 +12,20 @@ train.get("/", function(req, res) {
   //var trains = schedule.filter(train => train.route === route).map(item => item.train);
   res.send(trains);
 });
+train.put("/:train/:delay", function(req, res) {
+  var trainNumber= req.params.train;
+  var delay = req.params.delay;
+  var result = "Failed";
+  var justNumbers = schedule.map(item => item.train);
+  var index = justNumbers.indexOf(parseInt(trainNumber, 10));
+  if (index !== -1) {
+    schedule[index].variance = parseInt(delay, 10);
+    result = schedule[index];
+    res.send(result);
+  } else {
+    res.send(result);
+  }
+
+});
 
 module.exports = train;
