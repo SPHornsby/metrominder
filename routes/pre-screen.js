b78f8d0aa8c1eea3a3a4496bcf7f502d21835910
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-12 10:48:59 -0700

    Switches scheduled time to moment-timezone. #12

--- routes/pre-screen.js
+++ routes/pre-screen.js
@@ -5,7 +5,7 @@ var preScreen = function (req, res, next) {
   req.trains = schedule.map((train) => {
     return train.stops.map((stop) => {
       var momentTime = moment(stop.time).utcOffset(-7);
-      return {train: train.train, route: train.route, status: train.variance/60000, station: stop.name, time: stop.time, actualTime:momentTime.add(train.variance, "ms")};
+      return {train: train.train, route: train.route, status: train.variance/60000, station: stop.name, time: moment(stop.time).utcOffset(-7), actualTime:momentTime.add(train.variance, "ms")};
     });
   }).reduce((a,b) => a.concat(b))
   .filter(function(train) {
