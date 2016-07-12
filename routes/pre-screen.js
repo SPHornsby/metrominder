852f388ee60b472415ceb5e4a47ce0c5fad58a87
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-12 11:10:27 -0700

    Removes utc offset.

--- routes/pre-screen.js
+++ routes/pre-screen.js
@@ -4,8 +4,8 @@ var moment = require("moment-timezone");
 var preScreen = function (req, res, next) {
   req.trains = schedule.map((train) => {
     return train.stops.map((stop) => {
-      var momentTime = moment(stop.time).utcOffset(-7);
-      return {train: train.train, route: train.route, status: train.variance/60000, station: stop.name, time: moment(stop.time).utcOffset(-7), actualTime:momentTime.add(train.variance, "ms")};
+      var momentTime = moment(stop.time);
+      return {train: train.train, route: train.route, status: train.variance/60000, station: stop.name, time: moment(stop.time), actualTime:momentTime.add(train.variance, "ms")};
     });
   }).reduce((a,b) => a.concat(b))
   .filter(function(train) {
